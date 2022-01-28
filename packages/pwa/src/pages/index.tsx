8d343876c95282e47c280b3224ee722577c92724
flextremedev <p.wegner1992@gmail.com>
2022-01-28 22:15:19 +0100

    Add pwa package

---
+++ packages/pwa/src/pages/index.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+        <p>
+          Get started by editing <code>pages/index.tsx</code>
+        </p>
+      </main>
+    </div>
+  );
+}
