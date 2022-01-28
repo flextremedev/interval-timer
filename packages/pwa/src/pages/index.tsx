e9d04e9cc9367c0b47584e9912eb24bdaa180d48
flextremedev <p.wegner1992@gmail.com>
2022-01-28 22:33:35 +0100

    Add tailwind

--- packages/pwa/src/pages/index.tsx
+++ packages/pwa/src/pages/index.tsx
@@ -1,14 +1,12 @@
 export default function Home() {
   return (
-    <div>
-      <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-        <p>
-          Get started by editing <code>pages/index.tsx</code>
-        </p>
-      </main>
-    </div>
+    <main>
+      <h1 className="text-6xl">
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+      <p>
+        Get started by editing <code>pages/index.tsx</code>
+      </p>
+    </main>
   );
 }
