deec967f6ac30c5e52f125509e388466e8869994
Pascal Wegner <pascal.wegner@edith.care>
2022-02-05 14:52:25 +0100

    Connect timer machine to form

--- packages/pwa/src/pages/index.tsx
+++ packages/pwa/src/pages/index.tsx
@@ -1,6 +1,30 @@
 import { FormFields } from '../components/FormFields/FormFields';
+import { timerMachine } from '@interval-timer/core';
+import { useMachine } from '@xstate/react';
 
 export default function Home() {
+  const [state, send] = useMachine(timerMachine);
+
+  const setRounds = (rounds: number) => {
+    send({ type: 'SET_ROUNDS', rounds: Number(rounds) });
+  };
+
+  const setWorkInterval = (workInterval: Date) => {
+    send({
+      type: 'SET_WORK_INTERVAL',
+      workInterval,
+    });
+  };
+
+  const setBreakInterval = (breakInterval: Date) => {
+    send({
+      type: 'SET_BREAK_INTERVAL',
+      breakInterval,
+    });
+  };
+
+  const { breakInterval, rounds, workInterval } = state.context;
+
   return (
     <>
       <header />
@@ -8,12 +32,12 @@ export default function Home() {
         <div className="h-full flex flex-col items-stretch bg-blue-600">
           <div className="flex flex-col justify-center flex-1 bg-white rounded-b-3xl">
             <FormFields
-              rounds={1}
-              onRoundsChange={(rounds) => console.log({ rounds })}
-              breakInterval={new Date()}
-              onBreakIntervalChange={(date) => console.log(date)}
-              workInterval={new Date()}
-              onWorkIntervalChange={(date) => console.log(date)}
+              rounds={rounds}
+              onRoundsChange={setRounds}
+              breakInterval={breakInterval}
+              onBreakIntervalChange={setBreakInterval}
+              workInterval={workInterval}
+              onWorkIntervalChange={setWorkInterval}
             ></FormFields>
           </div>
           <div className="flex flex-col items-center flex-[0.25] pt-8 ">
