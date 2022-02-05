959cdbd0c6a87fbad47b791d85605b61c70888a8
Pascal Wegner <pascal.wegner@edith.care>
2022-02-05 22:41:03 +0100

    Export timerStates type

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,4 +2,4 @@ export * from './hooks';
 export * from './utils';
 export { timerMachine } from './machine/timerMachine';
 export * from './machine/types';
-export { timerStates } from './model/timerStates';
+export * from './model/timerStates';
